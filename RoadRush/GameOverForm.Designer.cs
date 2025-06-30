086197158893417f1cdbe9e5597c642955cb6d93
Thridev Maharajh <maharajhthridev@gmail.com>
2025-06-30 21:52:30 +0200

    GameOver Form Added and controls have been coded

---
+++ RoadRush/GameOverForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace RoadRush
+{
+    partial class GameOverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameOverForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PlayAgain = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(274, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GAME OVER";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(299, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PlayAgain
+            // 
+            this.PlayAgain.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayAgain.Location = new System.Drawing.Point(299, 213);
+            this.PlayAgain.Name = "PlayAgain";
+            this.PlayAgain.Size = new System.Drawing.Size(207, 55);
+            this.PlayAgain.TabIndex = 5;
+            this.PlayAgain.Text = "PLAY AGAIN";
+            this.PlayAgain.UseVisualStyleBackColor = true;
+            this.PlayAgain.Click += new System.EventHandler(this.PlayAgain_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(299, 336);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(207, 55);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "EXIT";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Menu
+            // 
+            this.Menu.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Menu.Location = new System.Drawing.Point(299, 275);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(207, 55);
+            this.Menu.TabIndex = 7;
+            this.Menu.Text = "MENU";
+            this.Menu.UseVisualStyleBackColor = true;
+            this.Menu.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // GameOverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.PlayAgain);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "GameOverForm";
+            this.Text = "GameOverForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button PlayAgain;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Menu;
+    }
+}
---
+++ RoadRush/GameOverForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoadRush
+{
+    public partial class GameOverForm : Form
+    {
+        public GameOverForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgain_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Hide game over screen
+
+            RacingGameForm newGame = new RacingGameForm();
+            newGame.FormClosed += (s, args) => this.Close(); // Close GameOverForm when game ends
+            newGame.Show();
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            MainMenuForm menu = new MainMenuForm();
+            menu.FormClosed += (s, args) => this.Close(); // Close GameOverForm when menu is closed
+            menu.Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
